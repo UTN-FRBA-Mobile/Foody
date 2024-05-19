91699a17fc5a12769d6508c10fbedb0779a44760
walterbarreiro <barreirowalter@live.com.ar>
2024-05-19 01:39:31 -0300

    Add logout button

--- android/app/src/main/java/ar/edu/utn/frba/foody/ui/main/AppScaffold.kt
+++ android/app/src/main/java/ar/edu/utn/frba/foody/ui/main/AppScaffold.kt
@@ -1,24 +1,34 @@
 package ar.edu.utn.frba.foody.ui.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import ar.edu.utn.frba.foody.R
+import ar.edu.utn.frba.foody.ui.navigation.AppScreens
 import ar.edu.utn.frba.foody.ui.theme.FoodyTheme
 
 @Composable
 fun AppScaffold(
+    navController: NavController,
     title: String? = null,
-    navController: NavController? = null,
     content: @Composable (PaddingValues) -> Unit) {
     val navigationIcon: (@Composable () -> Unit)? =
-        if (navController?.previousBackStackEntry != null) {
+        if (navController.previousBackStackEntry != null) {
             {
                 IconButton(onClick = {
                     navController.popBackStack()
@@ -32,6 +42,20 @@ fun AppScaffold(
             topBar = {
                 TopAppBar(
                     title = {
+                        Button(
+                            onClick = { navController.navigate(AppScreens.Login_Screen.route) },
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color.Transparent,
+                            ),
+                            elevation = ButtonDefaults.elevation(0.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.logout_icon),
+                                contentDescription = "Logout Icon",
+                                modifier = Modifier.size(24.dp),
+                                contentScale = ContentScale.FillBounds
+                            )
+                        }
                         Text(text = title ?: stringResource(id = R.string.app_name))
                     },
                     navigationIcon = navigationIcon,
@@ -45,4 +69,11 @@ fun AppScaffold(
             }
         )
     }
+}
+
+@Preview
+@Composable
+fun DefaultsPreview() {
+    val navController= rememberNavController()
+    AppScaffold(navController = navController, content = {})
 }
